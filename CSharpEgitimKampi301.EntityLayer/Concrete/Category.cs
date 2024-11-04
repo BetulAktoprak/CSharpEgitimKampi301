b451927b2b0ef59675c4169f8ea455a8f0fd959d
BetulAktoprak <145235075+BetulAktoprak@users.noreply.github.com>
2024-11-04 20:02:43 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    //Access Modifier ---> internal, public, private, protected
+    public class Category
+    {
+        //Field
+        //int x;
+
+        //Property
+        //public int x { get; set; }
+
+        //Variable
+        //void test()
+        //{
+        //    int x;
+        //}
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool Status { get; set; }
+    }
+}
